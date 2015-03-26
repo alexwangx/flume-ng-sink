8fc8dde1898c8c7447d0e44c5085045c83bf43db
wang xin <wangxin.dt@gmail.com>
2015-03-26 11:27:59 +0800

    Support multi-topic

--- README.md
+++ README.md
@@ -4,18 +4,25 @@ flume-ng-kafka-sink
 This project is used for [flume-ng](https://github.com/apache/flume) to communicate with [kafka 0.7.1](http://kafka.apache.org/07/quickstart.html).
 
 Configuration of Kafka Sink
-----------
+=============
+| Property Name | Default   | Description         |
+| ------------- | :-----:   | :----------         |
+| Type          |    -      |                     |
+| batchSize     |    100    | The max number of events to send to the kafka|
+| isJson        |   true    | format event to json    |
+| eventEncode   |   UTF-8   | event encoding |
 
+* Example
+```
     agent_log.sinks.sink_test.type = com.sink.kafka.KafkaSink
     agent_log.sinks.sink_test.batchSize = 100
     agent_log.sinks.sink_test.isJson = true
     agent_log.sinks.sink_test.eventEncode = GBK
     #kafka configuration
-    agent_log.sinks.sink_test.topic = test_topic
     agent_log.sinks.sink_test.zk.connect = 127.0.0.1:2181
     agent_log.sinks.sink_test.producer.type = async
     agent_log.sinks.sink_test.serializer.class = kafka.serializer.StringEncoder
-
+```
 
 isJson = true
 ----------
--- src/main/java/com/sink/kafka/KafkaSink.java
+++ src/main/java/com/sink/kafka/KafkaSink.java
@@ -21,64 +21,57 @@ package com.sink.kafka;
 import com.alibaba.fastjson.JSON;
 import kafka.javaapi.producer.Producer;
 import kafka.javaapi.producer.ProducerData;
-
-import org.apache.flume.Channel;
-import org.apache.flume.Context;
-import org.apache.flume.Event;
-import org.apache.flume.EventDeliveryException;
-import org.apache.flume.Transaction;
+import org.apache.flume.*;
 import org.apache.flume.conf.Configurable;
-import org.apache.flume.conf.ConfigurationException;
 import org.apache.flume.instrumentation.SinkCounter;
 import org.apache.flume.sink.AbstractSink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class KafkaSink extends AbstractSink implements Configurable {
     private static final Logger logger = LoggerFactory.getLogger(KafkaSink.class);
     private SinkCounter sinkCounter;
-    private String topic;
     private Producer<String, String> producer;
     private int batchSize;
     private String eventEncode;
     private boolean isJson;
-    final List<String> messagesList = new ArrayList<String>();
+    private Map<String, ArrayList<String>> map = new HashMap();
+    private Map<String, Long> timeoutMap = new HashMap();
 
     @Override
     public Status process() throws EventDeliveryException {
         Channel channel = getChannel();
         Transaction tx = channel.getTransaction();
         Status result = Status.READY;
-        Event event = null;
+        Event event;
+        String msg = null;
+        String topic = null;
         try {
             tx.begin();
-            for (int i = 0; i < batchSize; i++) {
-                event = channel.take();
-                if (event != null) {
-                    sinkCounter.incrementEventDrainAttemptCount();
-                    if (isJson) {
-                        FormatEvent formatEvent = new FormatEvent();
-                        formatEvent.setBody(new String(event.getBody(), eventEncode));
-                        formatEvent.setHost(event.getHeaders().get("hostip"));
-                        formatEvent.setTimestamp(Long.parseLong(event.getHeaders().get("timestamp")));
-                        formatEvent.setPath(event.getHeaders().get("path"));
-                        formatEvent.setTopic(topic);
-                        messagesList.add(JSON.toJSONString(formatEvent));
-                    } else {
-                        messagesList.add(new String(event.getBody(), eventEncode));
-                    }
+            event = channel.take();
+            if (event != null) {
+                sinkCounter.incrementEventDrainAttemptCount();
+                topic = event.getHeaders().get("topic");
+                if (isJson) {
+                    FormatEvent formatEvent = new FormatEvent();
+                    formatEvent.setBody(new String(event.getBody(), eventEncode));
+                    formatEvent.setHost(event.getHeaders().get("hostip"));
+                    formatEvent.setTimestamp(Long.parseLong(event.getHeaders().get("timestamp")));
+                    formatEvent.setPath(event.getHeaders().get("path"));
+                    formatEvent.setTopic(topic);
+                    msg = JSON.toJSONString(formatEvent);
                 } else {
-                    result = Status.BACKOFF;
-                    break;
+                    msg = new String(event.getBody(), eventEncode);
                 }
+            } else {
+                result = Status.BACKOFF;
             }
-            producer.send(new ProducerData<String, String>(topic, messagesList));
+            dealWithLog(msg,topic);
             tx.commit();
-            sinkCounter.addToEventDrainSuccessCount(messagesList.size());
-            messagesList.clear();
             return result;
         } catch (Exception e) {
             try {
@@ -94,12 +87,30 @@ public class KafkaSink extends AbstractSink implements Configurable {
         }
     }
 
+    private void dealWithLog(String msg,String topic) {
+        if(msg == null || topic == null) return;
+        if (map.containsKey(topic)) {
+            if (map.get(topic).size() < 1) {
+                timeoutMap.put(topic,System.currentTimeMillis());
+            }
+            map.get(topic).add(msg);
+        }else {
+            ArrayList list = new ArrayList();
+            list.add(msg);
+            map.put(topic, list);
+            timeoutMap.put(topic,System.currentTimeMillis());
+        }
+
+        if (map.get(topic).size() >= batchSize || (System.currentTimeMillis() - timeoutMap.get(topic).longValue() > 5000)) {
+            producer.send(new ProducerData<String, String>(topic, map.get(topic)));
+            sinkCounter.addToEventDrainSuccessCount(map.get(topic).size());
+            map.put(topic, new ArrayList());
+        }
+
+    }
+
     @Override
     public void configure(Context context) {
-        topic = context.getString("topic");
-        if (topic == null) {
-            throw new ConfigurationException("Kafka topic must be specified.");
-        }
         this.batchSize = context.getInteger("batchSize", 100);
         this.eventEncode = context.getString("eventEncode", "UTF-8");
         this.isJson = context.getBoolean("isJson", true);
@@ -119,10 +130,6 @@ public class KafkaSink extends AbstractSink implements Configurable {
 
     @Override
     public synchronized void stop() {
-        if (!messagesList.isEmpty()) {
-            producer.send(new ProducerData<String, String>(topic, messagesList));
-        }
-        messagesList.clear();
         producer.close();
         sinkCounter.stop();
         super.stop();
